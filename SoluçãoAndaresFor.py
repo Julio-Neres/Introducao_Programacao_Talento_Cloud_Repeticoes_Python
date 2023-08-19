39758e1d6fbb7a6742d998990ee4b781870a0d36
Julio-Neres <142518640+Julio-Neres@users.noreply.github.com>
2023-08-19 11:06:21 -0300

    Add files via upload
    Bom dia,
    Segue as duas soluções encontras para o problema enviado:
    Precisamos imprimir um número para cada andar de um hotel de 20 andares. Porém, o dono do hotel é supersticioso e optou por não ter um 13ro andar.
    
    Escreva um código que imprima todos os números exceto o número 13.
    Escreva mais dois códigos que resolvam o mesmo problema, mas dessa vez usando os outros dois tipos de laços de repetição aprendidos.
    
    Como desafio, imprima eles em ordem decrescente (20, 19, 18...)
    
    Trabalhe esse código em seu IDE, suba ele para sua conta no GitHub e compartilhe o link desse projeto no campo ao lado para que outros desenvolvedores possam analisá-lo.

---
+++ SoluçãoAndaresFor.py
@@ -0,0 +1,9 @@
+andares= 20
+
+for i in range(20,0,-1):
+    andares = andares -1
+    if(andares==12):
+        continue
+    if(andares==-1):
+        break
+    print(i)
---
+++ SoluçãoAndaresWhile.py
@@ -0,0 +1,5 @@
+andares = 20  
+while andares >= 1: 
+    if andares != 13: 
+        print(andares)  
+    andares -= 1  
